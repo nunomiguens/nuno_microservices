ee6b2d08e79d0a9e4ac44121fec28c1c461dd439
nunomiguens <nunomiguens@gmail.com>
2022-10-04 00:27:19 +0100

    Inicial commit

---
+++ calculator/src/main/java/pt/com/nuno/foo/BarApplication.java
@@ -0,0 +1,13 @@
+package pt.com.nuno.foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarApplication.class, args);
+	}
+
+}
